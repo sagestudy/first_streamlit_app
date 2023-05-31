3c7f46843b0c343899812d0bd5c74ba8319aeb5f
sagestudy <134918158+sagestudy@users.noreply.github.com>
2023-05-31 12:26:33 +0530

    Content from Fruityvice Website

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -9,14 +10,23 @@ streamlit.text("🥗 Kale, Spinach & Rocket Smoothie")
 streamlit.text("🐔 Hard-Boiled Free Range Egg")
 streamlit.text("🥑🍞 Avocado Toast")
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+streamlit.header("🍌🥭 Build Your Own Fruit Smoothie 🥝🍇")
+my_fruit_list = pandas.read_csv(
+    "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt"
+)
+my_fruit_list = my_fruit_list.set_index("Fruit")
 
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"])
+# Let's put a pick list here so they can pick the fruit they want to include
+fruits_selected = streamlit.multiselect(
+    "Pick some fruits:", list(my_fruit_list.index), ["Avocado", "Strawberries"]
+)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 # print(fruits_to_show)
 if fruits_to_show.size == 0:
     fruits_to_show = my_fruit_list.copy()
 streamlit.dataframe(fruits_to_show)
+
+# New Section to show contents from fruityvice website...
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json())
