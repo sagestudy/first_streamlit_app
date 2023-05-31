500c4661b15374980f0e748aba9c6aab83b0e907
sagestudy <134918158+sagestudy@users.noreply.github.com>
2023-05-31 12:52:32 +0530

    Print the user choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,7 +29,9 @@ streamlit.dataframe(fruits_to_show)
 # New Section to show contents from fruityvice website...
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice = streamlit.text_input("What fruit would you like information about?", "all")
+streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
 # streamlit.text(fruityvice_response.json())
 
 # Converts the JSON from the response into a dataframe in table format...
