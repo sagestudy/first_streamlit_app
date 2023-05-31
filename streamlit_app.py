1ade5b06e1a1508dcfa139608a8e0b1aca330a17
sagestudy <134918158+sagestudy@users.noreply.github.com>
2023-05-31 16:40:21 +0530

    Add requirements file and update import in streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 import pandas
 import requests
+import snowflake.connector
 
 streamlit.title("My Parents New Healthy Diner")
 
@@ -28,8 +29,10 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to show contents from fruityvice website...
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input("What fruit would you like information about?", "all")
-streamlit.write('The user entered ', fruit_choice)
+fruit_choice = streamlit.text_input(
+    "What fruit would you like information about?", "all"
+)
+streamlit.write("The user entered ", fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # streamlit.text(fruityvice_response.json())
